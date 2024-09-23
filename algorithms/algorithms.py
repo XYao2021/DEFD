a16cc6f05128259e1dc05020751e6d5cd0c1a9cb
Xin Yao <90070559+XYao2021@users.noreply.github.com>
2024-09-23 14:21:37 -0400

    Add files via upload

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -9,7 +9,8 @@ class Algorithms:
                  client_residuals=None, client_accumulates=None, client_compressors=None,
                  models=None, data_loaders=None, transfer=None, neighbor_models=None,
                  neighbors_accumulates=None, client_tmps=None, neighbors_estimates=None,
-                 client_partition=None, control=False, alpha_max=None, compression_method=None):
+                 client_partition=None, control=False, alpha_max=None, compression_method=None,
+                 estimate_gossip_error=None, current_weights=None, m_hat=None):
         super().__init__()
         self.algorithm_name = name
         self.local_iter = iter_round
@@ -31,6 +32,10 @@ class Algorithms:
         self.neighbors_estimates = neighbors_estimates
         self.client_partition = client_partition
         self.client_history = self.client_residuals
+
+        self.estimate_gossip_error = estimate_gossip_error
+        self.current_weights = current_weights
+        self.m_hat = m_hat
 
         self.control = control
 
@@ -101,66 +106,13 @@ class Algorithms:
         else:
             return False
 
-    def EFD(self, iter_num):
-        Averaged_weights = self._average_updates_EFD(updates=self.neighbor_models, update=self.client_weights)  # X_t(W-I)
-        alpha = []
-        for n in range(self.num_clients):
-            if self.control:
-                qt = self.client_partition[n].get_q(iter_num)
-                if np.random.binomial(1, qt) == 1:
-                    Vector_update = self._training(data_loader=self.data_loaders[n], client_weights=self.client_weights[n], model=self.models[n])
-                    Vector_update -= self.client_weights[n]  # gradient
-                    Vector_update += Averaged_weights[n]
-                else:
-                    # pass
-                    Vector_update = Averaged_weights[n]
-            else:
-                Vector_update = self._training(data_loader=self.data_loaders[n], client_weights=self.client_weights[n], model=self.models[n])
-                Vector_update -= self.client_weights[n]  # gradient
-                Vector_update += Averaged_weights[n]
-
-            # Vector_update -= self.client_weights[n]  # Difference between averaged weights and local weights
-            Vector_update -= self.client_history[n]
-
-            # current_gradient = Vector_update
-            # current_norm = torch.sum(torch.square(Vector_update)).item()
-            # past_norm = torch.sum(torch.square(Vector_update + self.client_residuals[n])).item()
-            # if past_norm == 0:
-            #     current_vs_past = 0
-            # else:
-            #     current_vs_past = current_norm / past_norm
-            # change_ratio.append(current_vs_past)
-
-            bt_norm = torch.sum(torch.square(Vector_update + self.client_residuals[n])).item()
-            Vector_update, self.client_residuals[n] = self.client_compressor[n].get_trans_bits_and_residual(iter=iter_num, w_tmp=Vector_update,
-                                                                                                            w_residual=self.client_residuals[n],
-                                                                                                            device=self.device, neighbors=self.neighbors[n])
-
-            residual_norm = torch.sum(torch.square(self.client_residuals[n])).item()
-            current_alpha = residual_norm / bt_norm
-            alpha.append(current_alpha)
-
-            self.client_weights[n] += Vector_update
-            self.client_history[n] = Averaged_weights[n]
-            for m in range(self.num_clients):
-                if n in self.neighbors[m]:
-                    self.neighbor_models[m][self.neighbors[m].index(n)] += Vector_update
-
-        # if self._check_weights(self.client_weights, self.neighbor_models):
-        #     print(iter_num, 'ALGORITHM SUCCESS THIS ROUND')
-        # else:
-        #     raise Exception('ALGORITHM FAIL')
-
-        # print(alpha)
-        if len(alpha) > 0:
-            self.Alpha.append(sum(alpha)/len(alpha))
-
     def EFD_dc(self, iter_num):
         # Averaged_weights = self._average_updates_EFD(updates=self.neighbor_models, update=self.client_weights)  # X_t(W-I)
         Averaged_weights = self._average_updates(updates=self.neighbor_models)
         alpha = []
         maxes = []
         for n in range(self.num_clients):
+            # print(iter_num, self.models[n].learning_rate)
             if self.control:
                 qt = self.client_partition[n].get_q(iter_num)
                 if np.random.binomial(1, qt) == 1:
@@ -178,108 +130,19 @@ class Algorithms:
 
             Vector_update -= self.client_weights[n]  # Difference between averaged weights and local weights
 
-            bt_norm = torch.sum(torch.square(Vector_update + self.client_residuals[n])).item()
-            maxes.append(max(bt_norm))
+            # bt_norm = torch.sum(torch.square(Vector_update + self.client_residuals[n])).item()
+            # maxes.append(bt_norm)
 
             Vector_update, self.client_residuals[n] = self.client_compressor[n].get_trans_bits_and_residual(iter=iter_num, w_tmp=Vector_update, w_residual=self.client_residuals[n], device=self.device, neighbors=self.neighbors[n])
 
-            residual_norm = torch.sum(torch.square(self.client_residuals[n])).item()
-            current_alpha = residual_norm / bt_norm
-            alpha.append(current_alpha)
+            # residual_norm = torch.sum(torch.square(self.client_residuals[n])).item()
+            # current_alpha = residual_norm / bt_norm
+            # alpha.append(current_alpha)
 
             self.client_weights[n] += Vector_update
             for m in range(self.num_clients):
                 if n in self.neighbors[m]:
                     self.neighbor_models[m][self.neighbors[m].index(n)] += Vector_update
-
-        self.max.append(sum(maxes)/len(maxes))
-
-        # if self._check_weights(self.client_weights, self.neighbor_models):
-        #     print(iter_num, 'ALGORITHM SUCCESS THIS ROUND')
-        # else:
-        #     raise Exception('ALGORITHM FAIL')
-
-        # print(alpha)
-        if len(alpha) > 0:
-            self.Alpha.append(sum(alpha) / len(alpha))
-
-    def _averaged_choco(self, updates, update):
-        Averaged = []
-        for i in range(self.num_clients):
-            summation = torch.zeros_like(update[0])
-            for j in range(len(updates[i])):
-                summation += (1/len(updates[i])) * (updates[i][j] - update[i])
-            Averaged.append(summation)
-        return Averaged
-
-    def CHOCO(self, iter_num, consensus):
-
-        Averaged_accumulate = self._averaged_choco(updates=self.neighbor_accumulates, update=self.client_accumulates)
-
-        for n in range(self.num_clients):
-            if self.control:
-                qt = self.client_partition[n].get_q(iter_num)
-                if np.random.binomial(1, qt) == 1:
-                    self.client_weights[n] = self.client_tmps[n] + consensus * Averaged_accumulate[n]
-                    self.client_tmps[n] = self._training(data_loader=self.data_loaders[n], client_weights=self.client_weights[n], model=self.models[n])
-                else:
-                    self.client_weights[n] = self.client_tmps[n]
-                    self.client_tmps[n] = self.client_weights[n]
-            else:
-                self.client_weights[n] = self.client_tmps[n] + consensus * Averaged_accumulate[n]
-                self.client_tmps[n] = self._training(data_loader=self.data_loaders[n], client_weights=self.client_weights[n], model=self.models[n])
-
-            Vector_update = self.client_weights[n] - self.client_accumulates[n]
-            Vector_update, _ = self.client_compressor[n].get_trans_bits_and_residual(w_tmp=Vector_update, iter=iter_num,
-                                                                                     w_residual=self.client_residuals[n],
-                                                                                     device=self.device, neighbors=self.neighbors[n])
-            self.client_accumulates[n] += Vector_update  # Vector Update is q_t
-            for m in range(self.num_clients):
-                if n in self.neighbors[m]:
-                    self.neighbor_accumulates[m][self.neighbors[m].index(n)] += Vector_update
-
-        if iter_num % 200 == 0:
-            if consensus > 0.1:
-                consensus -= 0.1
-            else:
-                consensus = consensus
-
-    # def CHOCO_E(self, iter_num, consensus):  # Need to be revised
-    #
-    #     Averaged_accumulate = self._averaged_choco(updates=self.neighbor_accumulates, update=self.client_accumulates)
-    #
-    #     for n in range(self.num_clients):
-    #         if iter_num >= 1:
-    #             self.client_history[n] = self.client_weights[n]
-    #         if self.control:
-    #             print(self.control)
-    #             qt = self.client_partition[n].get_q(iter_num)
-    #             if np.random.binomial(1, qt) == 1:
-    #                 self.client_weights[n] = self.client_tmps[n] + consensus * Averaged_accumulate[n]
-    #                 self.client_tmps[n] = self._training(data_loader=self.data_loaders[n],
-    #                                                      client_weights=self.client_weights[n], model=self.models[n])
-    #             else:
-    #                 self.client_weights[n] = self.client_tmps[n]
-    #                 self.client_tmps[n] = self.client_weights[n]
-    #         else:
-    #             self.client_weights[n] = self.client_tmps[n] + consensus * Averaged_accumulate[n]
-    #             self.client_tmps[n] = self._training(data_loader=self.data_loaders[n],
-    #                                                  client_weights=self.client_weights[n], model=self.models[n])
-    #
-    #         Vector_update = self.client_weights[n] - self.client_history[n]
-    #         Vector_update, self.client_residuals[n] = self.client_compressor[n].get_trans_bits_and_residual(w_tmp=Vector_update, iter=iter_num,
-    #                                                                                                         w_residual=self.client_residuals[n],
-    #                                                                                                         device=self.device, neighbors=self.neighbors[n])
-    #         self.client_accumulates[n] += Vector_update  # Vector Update is q_t
-    #         for m in range(self.num_clients):
-    #             if n in self.neighbors[m]:
-    #                 self.neighbor_accumulates[m][self.neighbors[m].index(n)] += Vector_update
-    #
-    #         if iter_num % 200 == 0:
-    #             if consensus > 0.1:
-    #                 consensus -= 0.1
-    #             else:
-    #                 consensus = consensus
 
     def DCD(self, iter_num):
         Averaged_weights = self._average_updates(updates=self.neighbor_models)
@@ -346,4 +209,113 @@ class Algorithms:
             for m in range(self.num_clients):
                 if n in self.neighbors[m]:
                     self.neighbors_estimates[m][self.neighbors[m].index(n)] = (1 - (2/iter_num)) * self.neighbors_estimates[m][self.neighbors[m].index(n)] + (2/iter_num) * z_vector
+
+    def _averaged_choco(self, updates, update):
+        Averaged = []
+        for i in range(self.num_clients):
+            summation = torch.zeros_like(update[0])
+            for j in range(len(updates[i])):
+                summation += (1/len(updates[i])) * (updates[i][j] - update[i])
+            Averaged.append(summation)
+        return Averaged
+
+    def CHOCO(self, iter_num, consensus):
+        # print(consensus)
+
+        Averaged_accumulate = self._averaged_choco(updates=self.neighbor_accumulates, update=self.client_accumulates)
+
+        for n in range(self.num_clients):
+            if self.control:
+                qt = self.client_partition[n].get_q(iter_num)
+                if np.random.binomial(1, qt) == 1:
+                    self.client_weights[n] = self.client_tmps[n] + consensus * Averaged_accumulate[n]
+                    self.client_tmps[n] = self._training(data_loader=self.data_loaders[n], client_weights=self.client_weights[n], model=self.models[n])
+                else:
+                    self.client_weights[n] = self.client_tmps[n]
+                    self.client_tmps[n] = self.client_weights[n]
+            else:
+                self.client_weights[n] = self.client_tmps[n] + consensus * Averaged_accumulate[n]
+                self.client_tmps[n] = self._training(data_loader=self.data_loaders[n], client_weights=self.client_weights[n], model=self.models[n])
+
+            Vector_update = self.client_weights[n] - self.client_accumulates[n]
+            Vector_update, _ = self.client_compressor[n].get_trans_bits_and_residual(w_tmp=Vector_update, iter=iter_num,
+                                                                                     w_residual=self.client_residuals[n],
+                                                                                     device=self.device, neighbors=self.neighbors[n])
+            self.client_accumulates[n] += Vector_update  # Vector Update is q_t
+            for m in range(self.num_clients):
+                if n in self.neighbors[m]:
+                    self.neighbor_accumulates[m][self.neighbors[m].index(n)] += Vector_update
+
+        # if iter_num % 200 == 0:
+        #     if consensus > 0.1:
+        #         consensus -= 0.1
+        #     else:
+        #         consensus = consensus
+
+    def AdaG_SGD(self, iter_num, beta, consensus, epsilon):  # x_hat_0 = 0 / u_i_0 = 0 U_t_i = error / AdaG needs very small consensus (gamma)
+        # The original algorithm possibly has errors of the update rule in the paper, not consistent with code
+
+        for n in range(self.num_clients):
+            self.client_tmps[n] = self._training(data_loader=self.data_loaders[n], client_weights=self.client_weights[n], model=self.models[n])
+
+            Vector_update = self.client_tmps[n] - self.client_accumulates[n]
+            Vector_update, _ = self.client_compressor[n].get_trans_bits_and_residual(w_tmp=Vector_update, iter=iter_num,
+                                                                                     w_residual=self.client_residuals[n],
+                                                                                     device=self.device,
+                                                                                     neighbors=self.neighbors[n])
+            self.client_accumulates[n] += Vector_update  # Vector Update is q_t
+            for m in range(self.num_clients):
+                if n in self.neighbors[m]:
+                    self.neighbor_accumulates[m][self.neighbors[m].index(n)] += Vector_update
+
+        Averaged_accumulate = self._averaged_choco(updates=self.neighbor_accumulates, update=self.client_accumulates)
+
+        for l in range(self.num_clients):
+            self.estimate_gossip_error[l] = (beta * self.estimate_gossip_error[l]) + ((1-beta) * torch.square(Averaged_accumulate[l]))
+
+            # adapt_factor = torch.sqrt(self.estimate_gossip_error[l]) + epsilon
+            adapt_factor = torch.sqrt(self.estimate_gossip_error[l] / (1-(beta**iter_num))) + epsilon
+            adapted_consensus = consensus / adapt_factor
+            adapted_consensus = torch.clamp(adapted_consensus, max=1.0)  # Problem: all ones, needs very small consensus
+
+            self.client_weights[l] = self.client_tmps[l] + adapted_consensus * Averaged_accumulate[l]
+            # self.client_weights[l] = self.client_weights[l] + adapted_consensus * Averaged_accumulate[l]  # Update rule in paper, not working
+
+    def Comp_QSADDLe(self, iter_num, rho, beta, learning_rate, consensus, mu):  # Cannot work with large learning rate
+
+        self.current_weights = self.client_weights
+        for n in range(self.num_clients):
+            trained_weights = self._training(data_loader=self.data_loaders[n], client_weights=self.client_weights[n], model=self.models[n])
+            gradients = -(trained_weights - self.client_weights[n]) / learning_rate
+
+            zeta = rho * (gradients / torch.norm(gradients))
+            trained_weights_1 = self._training(data_loader=self.data_loaders[n], client_weights=self.client_weights[n]+zeta, model=self.models[n])
+            gradients_1 = -(trained_weights_1 - (self.client_weights[n]+zeta)) / learning_rate
+
+            # m_t = beta * self.m_hat[n] + gradients
+            m_t = beta * self.m_hat[n] + gradients_1
+
+            self.client_tmps[n] = self.client_weights[n] - learning_rate * m_t
+
+        Averaged_accumulate = self._averaged_choco(updates=self.neighbor_accumulates, update=self.client_accumulates)
+
+        for n in range(self.num_clients):
+            self.client_weights[n] = self.client_tmps[n] + consensus * Averaged_accumulate[n]
+
+            distance = self.current_weights[n] - self.client_weights[n]
+            distance = distance / learning_rate  # actually is gradients
+
+            self.m_hat[n] = mu * self.m_hat[n] + (1-mu) * distance
+            Vector_update = self.client_weights[n] - self.client_accumulates[n]
+            Vector_update, _ = self.client_compressor[n].get_trans_bits_and_residual(w_tmp=Vector_update, iter=iter_num,
+                                                                                     w_residual=self.client_residuals[n],
+                                                                                     device=self.device,
+                                                                                     neighbors=self.neighbors[n])
+
+            self.client_accumulates[n] += Vector_update
+            for m in range(self.num_clients):
+                if n in self.neighbors[m]:
+                    self.neighbor_accumulates[m][self.neighbors[m].index(n)] += Vector_update
+
+
 
