40a1ecd0a76a7d6e7d0f36e886464de368dfd113
Xin Yao <90070559+XYao2021@users.noreply.github.com>
2025-03-19 11:30:59 -0400

    Add files via upload

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,13 +1,12 @@
 import os
 import sys
-from torchvision.datasets import FashionMNIST, MNIST
+from torchvision.datasets import FashionMNIST, MNIST, EMNIST, QMNIST, KMNIST, SVHN
 from torchvision.datasets import CIFAR10, CIFAR100
 import torchvision.transforms as transforms
 from pytorch_cinic.dataset import CINIC10
 from PIL import Image
 import torch
 import torchvision
-
 
 class FashionMNISTEnhanced(FashionMNIST):
     def __init__(self, root, train=True, transform=None, target_transform=None, download=False, device=None):
@@ -37,7 +36,119 @@ class FashionMNISTEnhanced(FashionMNIST):
         image, label = self.data_transformed[index], self.target_transformed[index]
         return image, label
 
+class EMNISTEnhanced(EMNIST):  # Need to reduce the number of inputs
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=False, device=None):
+        super().__init__(root, train, transform, target_transform, download)
+        self.data_transformed = []
+        self.target_transformed = []
+
+        for image in self.data:
+            image = Image.fromarray(image.numpy(), mode='L')  # Mode L means (8-bit pixels, black and white)
+            if self.transform is not None:
+                image = self.transform(image)
+            self.data_transformed.append(image)
+
+        for target in self.targets:
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+            self.target_transformed.append(target)
+
+        self.data_transformed = torch.stack(self.data_transformed)
+        self.target_transformed = torch.stack(self.target_transformed)
+
+        if device is not None:
+            self.data_transformed = self.data_transformed.to(device)
+            self.target_transformed = self.target_transformed.to(device)
+
+    def __getitem__(self, index):
+        image, label = self.data_transformed[index], self.target_transformed[index]
+        return image, label
+
+class QMNISTEnhanced(QMNIST):  # Need to reduce the number of inputs
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=False, device=None):
+        super().__init__(root, train, transform, target_transform, download)
+        self.data_transformed = []
+        self.target_transformed = []
+
+        for image in self.data:
+            image = Image.fromarray(image.numpy(), mode='L')  # Mode L means (8-bit pixels, black and white)
+            if self.transform is not None:
+                image = self.transform(image)
+            self.data_transformed.append(image)
+
+        for target in self.targets:
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+            self.target_transformed.append(target)
+
+        self.data_transformed = torch.stack(self.data_transformed)
+        self.target_transformed = torch.stack(self.target_transformed)
+
+        if device is not None:
+            self.data_transformed = self.data_transformed.to(device)
+            self.target_transformed = self.target_transformed.to(device)
+
+    def __getitem__(self, index):
+        image, label = self.data_transformed[index], self.target_transformed[index]
+        return image, label
+
+class KMNISTEnhanced(KMNIST):
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=False, device=None):
+        super().__init__(root, train, transform, target_transform, download)
+        self.data_transformed = []
+        self.target_transformed = []
+
+        for image in self.data:
+            image = Image.fromarray(image.numpy(), mode='L')  # Mode L means (8-bit pixels, black and white)
+            if self.transform is not None:
+                image = self.transform(image)
+            self.data_transformed.append(image)
+
+        for target in self.targets:
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+            self.target_transformed.append(target)
+
+        self.data_transformed = torch.stack(self.data_transformed)
+        self.target_transformed = torch.stack(self.target_transformed)
+
+        if device is not None:
+            self.data_transformed = self.data_transformed.to(device)
+            self.target_transformed = self.target_transformed.to(device)
+
+    def __getitem__(self, index):
+        image, label = self.data_transformed[index], self.target_transformed[index]
+        return image, label
+
 class MNISTEnhanced(MNIST):
+    def __init__(self, root, train=True, transform=None, target_transform=None, download=False, device=None):
+        super().__init__(root, train, transform, target_transform, download)
+        self.data_transformed = []
+        self.target_transformed = []
+
+        for image in self.data:
+            image = Image.fromarray(image.numpy(), mode='L')  # Mode L means (8-bit pixels, black and white)
+            if self.transform is not None:
+                image = self.transform(image)
+            self.data_transformed.append(image)
+
+        for target in self.targets:
+            if self.target_transform is not None:
+                target = self.target_transform(target)
+            self.target_transformed.append(target)
+
+        self.data_transformed = torch.stack(self.data_transformed)
+        self.target_transformed = torch.stack(self.target_transformed)
+
+        if device is not None:
+            self.data_transformed = self.data_transformed.to(device)
+            self.target_transformed = self.target_transformed.to(device)
+
+    def __getitem__(self, index):
+        image, label = self.data_transformed[index], self.target_transformed[index]
+        return image, label
+
+class SVHNEnhanced(SVHN):
     def __init__(self, root, train=True, transform=None, target_transform=None, download=False, device=None):
         super().__init__(root, train, transform, target_transform, download)
         self.data_transformed = []
@@ -96,6 +207,7 @@ class CIFAR10Enhanced(CIFAR10):
 
 def loading(dataset_name, data_path, device):
     # print(dataset_name, data_path)
+    # FashionMNIST, MNIST, EMNIST, QMNIST, KMNIST
     if dataset_name == 'FashionMNIST':
         transform = transforms.Compose([transforms.ToTensor(),
                                         transforms.Normalize((0.1307,), (0.3081,))])
@@ -108,6 +220,31 @@ def loading(dataset_name, data_path, device):
         train_data = MNISTEnhanced(data_path, transform=transform, download=True, device=device)
         test_data = MNISTEnhanced(data_path, train=False, transform=transform, device=device)
 
+    elif dataset_name == 'EMNIST':
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((0.1307,), (0.3081,))])
+        train_data = EMNISTEnhanced(data_path, transform=transform, download=True, device=device)
+        test_data = EMNISTEnhanced(data_path, train=False, transform=transform, device=device)
+
+    elif dataset_name == 'QMNIST':
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((0.1307,), (0.3081,))])
+        train_data = QMNISTEnhanced(data_path, transform=transform, download=True, device=device)
+        test_data = QMNISTEnhanced(data_path, train=False, transform=transform, device=device)
+
+    elif dataset_name == 'KMNIST':
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Normalize((0.1307,), (0.3081,))])
+        train_data = KMNISTEnhanced(data_path, transform=transform, download=True, device=device)
+        test_data = KMNISTEnhanced(data_path, train=False, transform=transform, device=device)
+
+    elif dataset_name == 'SVHN':
+        transform = transforms.Compose([transforms.ToTensor(),
+            transforms.Normalize(mean=[0.4377, 0.4438, 0.4728], std=[0.1980, 0.2010, 0.1970])])
+        # train_data = SVHNEnhanced(data_path, transform=transform, download=True, device=device)
+        # test_data = SVHNEnhanced(data_path, train=False, transform=transform, device=device)
+        train_data = torchvision.datasets.SVHN(root='./data', split='train', transform=transforms.ToTensor(), download=True)
+        test_data = torchvision.datasets.SVHN('./data', split='test', transform=transform, download=True)
     elif dataset_name == 'CIFAR10':
         transform = transforms.Compose([transforms.ToTensor(),
                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])
